5610e6c5d8ed8fd6b10f5224c39462a38ec152b6
dhavy614 <192528769+Ajmeriya@users.noreply.github.com>
2026-01-21 23:42:38 +0530

    ohky today's work done

--- frontend/src/components/Common/MapComponent.jsx
+++ frontend/src/components/Common/MapComponent.jsx
@@ -4,6 +4,7 @@ import L from 'leaflet'
 import { useEffect, useMemo } from 'react'
 
 // Fix for default marker icon
+//today's working done
 delete L.Icon.Default.prototype._getIconUrl
 L.Icon.Default.mergeOptions({
   iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png',
